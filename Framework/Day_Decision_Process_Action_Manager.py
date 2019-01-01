4dc9ae49289a14e8469560c2b5432ed99a9ae3fb
Wallace Carr <wallacecarr4@gmail.com>
2018-12-31 23:22:05 -0700

    update DDPA process

--- Framework/Day_Decision_Process_Action_Manager.py
+++ Framework/Day_Decision_Process_Action_Manager.py
@@ -1,8 +1,6 @@
-
 # from DM_Buy import DM_Buy
 # from Stock import Stock
 from Calendar_Tracker import Calendar_Tracker
-
 
 
 class Day_Decision_Process_Action_Manager:
@@ -14,8 +12,6 @@ class Day_Decision_Process_Action_Manager:
             self.instance_calendar_tracker = Calendar_Tracker()
             # self.chosen_statistics = Chosen_Statistics()
         return self.__instance
-
-
 
     def get_DM_Buy(self):
         return self.DM_Buy
@@ -33,17 +29,3 @@ class Day_Decision_Process_Action_Manager:
     # Process bought stock, canceling others, init bought - else update DDPA stats, proceeding to DDPS
     def DM_Bought_Assembly(self):
         return ''
-
-
-
-    # test_set1 = np.array([20.0, 20.2, 20.04, 20.30])
-    #
-    # print(create_pchg_value_list(test_set1))
-    #
-    # five_minute_set = [[20.0], [20.1], [20.15], [20.20], [20.3], [20.4], [20.41]]
-    # for val in five_minute_set:
-    #     if (calculate_pchg_delimiter_met(20.0, val)):
-    #         print("selling stock")
-    #     else:
-    #         print("value less than delimiter")
-
