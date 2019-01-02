0deaad8d5004f50f4354e06eee2b07e8900ebb47
Wallace Carr <wallacecarr4@gmail.com>
2019-01-01 23:43:44 -0700

    update composite store process

--- Framework/Stock_Composite.py
+++ Framework/Stock_Composite.py
@@ -2,40 +2,24 @@ import Stock
 
 
 class Stock_Composite:
-
-
-    def __init__(self,name):
+    def __init__(self, name):
         self.name = name
         self.list_stocks = []
+
     def get_list_stocks(self):
         return self.listStocks
 
     def get_name(self):
         return self.name
+
     def set_name(self, name):
         self.name = name
 
     def get_generation_iteration(self):
         return self.generationIteration
-    def set_generation_iteration(self,generationIteration):
+
+    def set_generation_iteration(self, generationIteration):
         self.generationIteration = generationIteration
 
     def get_list_stocks(self):
         return self.list_stocks
-
-
-    # def action2(self, *args ):
-    #     print(args)
-    #     count = 0
-    #     for arg in args:
-    #         print(count)
-    #         if(count==0):
-    #             param1 = arg
-    #         if(count==1):
-    #             param2 = arg
-    #         count = count+1
-    #     self.chaosFunction(param1,param2)
-    # def chaosFunction(self,stockComposite,intToAdd):
-    #     print(stockComposite.get_name())
-    #     print(intToAdd)
-
