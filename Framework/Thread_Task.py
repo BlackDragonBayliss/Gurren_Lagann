06b2981ac3ec56ac48cea2cef7751abcaf701dcb
Wallace Carr <wallacecarr4@gmail.com>
2019-01-05 23:38:00 -0700

    update Thread_Factory functionality

--- Framework/Thread_Task.py
+++ Framework/Thread_Task.py
@@ -14,37 +14,3 @@ class Thread_Task:
 
     def set_name(self, name):
         self.name = name
-
-    # def create_modified_thread(self):
-        #create thread
-
-        #add list of async func
-        # t = Thread(target=start_loop, args=(new_loop))
-        # t.start()
-        # begin event loop
-        #add list of async func
-
-
-
-    # def async_ method_caller():
-
-
-
-    # def aysnc_thread(payload):
-    #     new_loop = asyncio.new_event_loop()
-    #
-    #     # for each function listMethodContainer
-    #     t = Thread(target=start_loop, args=(new_loop))
-    #     t.start()
-    #     return jsonRequest
-    #
-    # def start_loop(loop):
-    #     print("hitme "+str(countLoop))
-    #     asyncio.set_event_loop(loop)
-    #
-    #
-    #     loop.run_forever()
-    #     print(countLoop + 1)
-    #     # countLoop += 1
-
-    # def
