ca16c467dadebceb732c593c8fef9c3f26d016a4
Wallace Carr <wallacecarr4@gmail.com>
2019-01-11 23:30:14 -0700

    update Operation_Center Moirae timer_loop process

--- Framework/Chosen_Data_Manager.py
+++ Framework/Chosen_Data_Manager.py
@@ -9,7 +9,7 @@ class Chosen_Data_Manager(Data_Manager):
         super().set_data_manager_type('Chosen')
 
     def init_data_processing(self):
-        self.perpetual_timer_data_pull.setup_timer_stock(3, 1000, super().data_pull, 'data_pull')
+        self.perpetual_timer_data_pull.setup_timer_stock(10, 1000, super().data_pull, 'data_pull')
 
     def test_print(self):
         print(super().get_data_manager_type())
